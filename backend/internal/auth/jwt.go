60b5046ac11cfe5b2f1b8ada0a9b85e8f7e62ffd
Abdullox <usmonkarimov0726@gmail.com>
2026-04-15 10:16:34 +0500

    checking if test&scan backend works

--- backend/internal/auth/jwt.go
+++ backend/internal/auth/jwt.go
@@ -17,13 +17,13 @@ func (s *AuthService) JWTMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			http.Error(w, "missing token", 400)
+			http.Error(w, "missing toke ", 400)
 			return
 		}
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			http.Error(w, "invalid token format", 400)
+			http.Error(w, "invalid token format ", 400)
 			return
 		}
 
